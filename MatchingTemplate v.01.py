b5160cdb80e0cce3b26d0ee26180e906a3f6b141
Andy Chang <achang@yext.com>
2017-11-30 16:25:28 -0500

    no message

---
+++ MatchingTemplate v.01.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 14:23:11 2017
+
+@author: achang
+"""
+
+from time import strftime
+import pandas as pd
+import sys
+import os 
+import xlrd 
+import numpy as np
+import re
+import xlsxwriter
+from fuzzywuzzy import fuzz  
+#from mypackages import FunctionToolbox as tbox
+
+
+#This function cleans the names 
+def cleanName(name):
+    
+    try:
+        name = name.strip().lower()
+        name = name.replace("&"," and ").replace("professional corporation","")
+        name = name.replace(" pc "," ").replace(" lp "," ").replace(" llc "," ")
+        name = name.replace("incorporated","").replace(" inc."," ").replace(" inc "," ")
+        name = re.sub('[^A-Za-z0-9\s]+', '', name)
+        name = re.sub( '\s+', ' ', name)
+    except AttributeError:
+        name = ""
+    return name
+
+#This function cleans the addresses
+def cleanAddress(address):
+    try:
+        address = address.strip().lower()
+        address = address.replace("&"," and ").replace(".","")
+        address = address.replace(" avenue "," ave ").replace(" boulevard "," blvd ").replace(" bypass "," byp ")
+        address = address.replace(" circle "," cir ").replace(" drive "," dr ").replace(" expressway "," expy ")
+        address = address.replace(" highway "," hwy ").replace(" parkway "," pkwy ").replace(" road "," rd ")
+        address = address.replace(" street "," st ").replace(" turnpike "," tpke ")
+        address = re.sub('[^A-Za-z0-9\s]+', '', address)
+        address = re.sub( '\s+', ' ', address)
+    except AttributeError:
+        address = ""
+    return address
+    
+#This function cleans the city
+def cleanCity(city):
+    try:
+        city = city.strip().lower()
+        city = city.replace("&"," and ").replace("saint ","st ")
+        city = re.sub('[^A-Za-z0-9\s]+', '', city)
+        city = re.sub( '\s+', ' ', city)
+    except AttributeError:
+        city = ""
+    return city
+
+#This function compares the names in the file
+def compareName(df):
+    df['Cleaned Location Name'] = df['Location Name'].apply(cleanName) 
+    df['Cleaned Listing Name'] = df['Listing Name'].apply(cleanName)
+    averagenamescore = []
+    for index, row in df.iterrows(): 
+        nsr = fuzz.ratio(row['Cleaned Location Name'], row['Cleaned Listing Name'])
+        ntpr = fuzz.partial_ratio(row['Cleaned Location Name'], row['Cleaned Listing Name'])
+        average = np.mean([nsr,ntpr])
+        averagenamescore.append(average)
+    df['Name Score'] = averagenamescore
+    
+#This function compares the countries in the file
+def compareStateCountry(df):
+    statematch = []
+    for index, row in df.iterrows(): 
+        # Drop different countries
+        if (row['Input Location Country'] != row['Duplicate Location Country']):
+            df.drop(index,inplace=True)
+        else:
+            # Flag international locations
+            if row['Input Location Country'] != "US":
+                statematch.append("International")
+            else:
+                # Drop different states within US
+                if (row['Location State'] != row['Listing State']):
+                    df.drop(index,inplace=True)
+                else:
+                    statematch.append("US")
+    df['US?'] = statematch
+
+#This function compares the locationIds in the file
+def compareId(df):
+    IDmatch = []
+    for index, row in df.iterrows(): 
+        if (row['Input Location Id'] == row['Duplicate Location Id']):
+            df.drop(index,inplace=True)
+        else:
+            IDmatch.append("Unique Pair")                
+    df['Location ID Match'] = IDmatch
+
+#This function removes matches that are not 1
+def userMatch(df):
+    for index, row in df.iterrows(): 
+        if (row['Match \n1 = yes, 0 = no'] != 1):
+            df.drop(index,inplace=True)
+
+#This function compares the phones in the file                
+def comparePhone(df):
+    df['Phone Match'] = df.apply(lambda x: '0' if x['Location Phone'] == x['Listing Phone'] else '1', axis=1)
+
+#This function compares the addresses in the file                
+def compareAddress(df):
+    df['Cleaned Input Address'] = df['Location Address'].apply(cleanAddress) 
+    df['Cleaned Listing Address'] = df['Listing Address'].apply(cleanAddress)
+    averageaddressscore = []
+    for index, row in df.iterrows(): 
+        asr = fuzz.ratio(row['Cleaned Input Address'], row['Cleaned Listing Address'])
+        averageaddressscore.append(asr)
+    df['Address Score'] = averageaddressscore            
+
+#This function compares the cities in the file                
+def compareCity(df):
+    df['Cleaned Input City'] = df['Location City'].apply(cleanCity) 
+    df['Cleaned Listing City'] = df['Listing City'].apply(cleanCity)
+    averagecityscore = []
+    for index, row in df.iterrows(): 
+        csr = fuzz.ratio(row['Cleaned Input City'], row['Cleaned Listing City'])
+        ctpr = fuzz.partial_ratio(row['Cleaned Input City'], row['Cleaned Listing City'])
+        
+        average = np.mean([csr,ctpr])
+        averagecityscore.append(average)   
+    df['City Score'] = averagecityscore
+
+#This function compares the Country in the file                        
+def compareCountry(df):
+     df['Country Match'] = df.apply(lambda x: '0' if x['Input Location Country'] == x['Duplicate Location Country'] else '1', axis=1)
+
+#This function compares the Zips in the file                   
+def compareZip(df):
+     df['Zip Match'] = df.apply(lambda x: '0' if x['Location Zip'] == x['Listing Zip'] else '1', axis=1)
+
+#This function compares the data by calling on all the functions
+def compareData(df):
+    #compareId(df)
+    comparePhone(df)
+    #compareCountry(df)
+    compareZip(df)
+    compareName(df)
+    compareAddress(df)
+    #compareStateCountry(df)
+    suggestedmatch(df)
+
+
+#This function provides a suggested match based on certain name/address thresholds
+def suggestedmatch(df):  
+    robotmatch = []
+    for index, row in df.iterrows(): 
+     #   print "."
+        #Check to see if matching location IDs
+ #       if row['Country'] != "US":
+  #          robotmatch.append("Check - Intl")
+  #      else:
+            #If Same Country and State Check the Names field
+        if row['Name Score'] <= 60:
+            robotmatch.append("No Match - Name")
+        else:
+            if row['Address Score'] < 70:
+                robotmatch.append("No Match - Address")
+            else:
+                if 60 < row['Name Score'] < 80:
+                    robotmatch.append("Check")
+                else:
+                    robotmatch.append("Match Suggested")                                                
+    df['Robot Suggestion'] = robotmatch
+    df['Match \n1 = yes, 0 = no'] = ""
+    
+def main():    
+    
+    """xls = raw_input("Incorrect file path. Enter correct path: ").lstrip("\"").rstrip("\"")
+    try:
+        df = pd.ExcelFile(xls).parse(ws)
+    except xlrd.XLRDError:
+        ws = raw_input("Enter worksheet name: ")
+        df = pd.ExcelFile(xls).parse(ws)
+except xlrd.XLRDError:
+    ws = raw_input("Enter worksheet name: ")
+    df = pd.ExcelFile(xls).parse(ws)
+
+    df = pd.ExcelFile(xls).parse(ws)
+
+    # Get filepath
+    xlsFile = raw_input("\nBefore you begin, "
+                        "please ensure that your file is saved as an XLSX "
+                        "\n\nEnter File Path Here: ").replace('""','').lstrip("\"").rstrip("\"")
+    directory = xlsFile[:xlsFile.rindex("\\")]                            
+    os.chdir(directory)"""
+    #probably different criteria for international data
+    xlsFile = r"C:\Users\storres\Desktop\test 3.xlsx"
+    df = pd.ExcelFile(xlsFile).parse("Sheet1")
+
+
+    #wb = xlrd.open_workbook(xlsFile, on_demand=True)
+    #sNames = wb.sheet_names()        
+    #for name in sNames:
+    #     wsTitle = name
+                  
+    row = 0 
+    # Read in data set
+    lastcol = len(df.columns)
+    
+    for x in df['Link ID']:
+        row += 1
+    compareData(df)
+            
+    FilepathMatch =  os.path.expanduser("~\Documents\Python Scripts\Matching Template Output.xlsx")
+
+    writer = pd.ExcelWriter(FilepathMatch, engine='xlsxwriter')
+    df.to_excel(writer,sheet_name='sheet1', index=False)
+    workbook  = writer.book
+    worksheet = writer.sheets['sheet1']
+
+    headerformat = workbook.add_format({
+    'bold': True,
+    'text_wrap': True})
+    for col_num, value in enumerate(df.columns.values):
+        worksheet.write(0, col_num, value, headerformat)
+        
+    try:
+        writer.save()    
+        print "\nDone! Results have been wrizzled to your Excel file. 1love <3"
+        print "\nChannel Conflict Matching here:"
+        print FilepathMatch 
+    except IOError:
+        print "\nIOError: Make sure your Excel file is closed before re-running the script."          
+            
+main()
